72c00c6a60b4d6685cdc49dfb2b17609da189849
Andrew Patterson <andrewpatterson@MacBook-Air-3001.local>
2017-04-23 15:23:21 -0700

    create password reset email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 	# user and email must be present and have max lengths. validates email format
   before_save :downcase_email
   before_create :create_activation_digest
@@ -51,6 +51,22 @@ class User < ApplicationRecord
   def send_activation_email
   	UserMailer.account_activation(self).deliver_now
   end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
   	# Converts email to all lower-case.
     def downcase_email
