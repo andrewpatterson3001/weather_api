3ad51dd6187d190567ddd5dbb3c313b94f8e6340
Andrew Patterson <andrewpatterson@MacBook-Air-3001.local>
2017-04-22 21:15:46 -0700

    remove comment to try and fix prod issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the App!"	
       redirect_to @user # same as redirect_to user_url(@user)
