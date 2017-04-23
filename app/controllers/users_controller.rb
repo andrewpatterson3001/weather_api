109f3dfcde30b3647ff94e56db87e61becf42f86
Andrew Patterson <andrewpatterson@MacBook-Air-3001.local>
2017-04-22 20:25:28 -0700

    handle successful and unsuccessful signups. enable ssl in prod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the App!"	
+      redirect_to @user # same as redirect_to user_url(@user)
     else
       render 'new'
     end
